14b29caf7cecd9f1510b4cb0658c5cbf2c41f3b7
Nicky <nickytermaat24051996@live.nl>
2016-05-13 10:50:03 +0200

    Modified Tweet, added variable, constructor and getter

--- app/src/main/java/com/nickytm/ap/twitterclient/Tweet.java
+++ app/src/main/java/com/nickytm/ap/twitterclient/Tweet.java
@@ -4,4 +4,14 @@ package com.nickytm.ap.twitterclient;
  * Created by Nicky on 13/05/2016.
  */
 public class Tweet {
+
+    private String tweet;
+
+    public Tweet(String tweet) {
+        this.tweet = tweet;
+    }
+
+    public String getTweet() {
+        return tweet;
+    }
 }
