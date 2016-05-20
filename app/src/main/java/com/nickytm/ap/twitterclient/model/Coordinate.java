9063e094527079e4f16e78832801ba95d9a6c963
Remy <Remye98@gmail.com>
2016-05-20 10:44:06 +0200

    Coordinate json constuctor

--- app/src/main/java/com/nickytm/ap/twitterclient/model/Coordinate.java
+++ app/src/main/java/com/nickytm/ap/twitterclient/model/Coordinate.java
@@ -1,12 +1,29 @@
 package com.nickytm.ap.twitterclient.model;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Coordinate {
 
-    private float coordinate;
+    private float coordinate[] = new float[2];
     private String type;
 
-    public Coordinate(float coordinates, String type) {
+    public Coordinate(float[] coordinates, String type) {
         this.coordinate = coordinates;
         this.type = type;
     }
+
+    public Coordinate(JSONArray jsonArray) throws JSONException {
+        this.coordinate[0] = jsonArray.getInt(0);
+        this.coordinate[1] = jsonArray.getInt(1);
+    }
+
+    public Coordinate(JSONObject jsonObject) throws JSONException{
+        this.type = jsonObject.getString("type");
+    }
+
 }
